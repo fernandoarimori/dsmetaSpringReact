2714130b72b2f57aa878c2fa03e8718d25ced54a
Fernando Arimori <fernando.c.arimori@gmail.com>
2022-09-05 23:24:01 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,21 @@
-import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
+import NotificationButton from "./components/NotificationButton";
 
 
 function App() {
-  return(
+  return (
     <>
-  <h1>Hello Woreqweqweweld!</h1>
-  <NotificationButton/>
-  </>
-  )
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
