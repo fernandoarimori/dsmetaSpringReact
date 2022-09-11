3ec80863f8a1ee27823bd0a41e40d2390a7bec9e
Fernando Arimori <fernando.c.arimori@gmail.com>
2022-09-11 12:46:39 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,8 +1,22 @@
 import icon from '../../assets/img/notificationIcon.svg'
+import axios from "axios";
 import './styles.css'
-function NotificationButton() {
+import { BASE_URL } from '../../utils/request';
+
+type Props = {
+  saleId: number;
+}
+
+function handleClick(id :number){
+  axios.get(`${BASE_URL}/sales/${id}/notification`)
+  .then(response => {
+    console.log("SUCESSO");
+  });
+}
+
+function NotificationButton( {saleId} : Props) {
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
     <img src={icon} alt="Notificar" />
   </div>
   )
